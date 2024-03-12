f41fabc4d20cfee349218239a63ef71adcf338d4
JustBecerra <justj.becerra@gmail.com>
2024-03-12 15:18:09 -0300

    remove tailwind from footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -21,22 +21,42 @@ export default function Footer() {
         gap: "1rem",
       }}
     >
-      <Typography className="md:!text-[1.25rem] min-[310px]:!text-[0.75rem] min-[310px]:w-[100%] text-center">
+      <Typography sx={{ fontSize: "1.25rem", textAlign: "center" }}>
         {t("ifyouwish")}
       </Typography>
       <Box
         sx={{ display: "flex", justifyContent: "space-around", width: "50%" }}
       >
         <Link href="https://github.com/JustBecerra" target="_blank">
-          <GitHubIcon className="!w-[2rem] !h-[2rem] rounded-full shadow-lg hover:shadow-white" />
+          <GitHubIcon
+            sx={{
+              width: "2rem",
+              height: "2rem",
+              borderRadius: "100%",
+              fill: "white",
+              "&:hover": {
+                fill: "rgb(8 145 178)",
+              },
+            }}
+          />
         </Link>
         <Link
           href="https://www.linkedin.com/in/justo-becerra-14868a1b4/"
           target="_blank"
         >
-          <LinkedInIcon className="!w-[2rem] !h-[2rem] rounded-lg shadow-lg hover:shadow-cyan-500" />
+          <LinkedInIcon
+            sx={{
+              width: "2rem",
+              height: "2rem",
+              borderRadius: "100%",
+              fill: "white",
+              "&:hover": {
+                fill: "rgb(8 145 178)",
+              },
+            }}
+          />
         </Link>
-        <Typography className="md:!text-[1.5rem] min-[310px]:!text-[0.75rem] !font-medium">
+        <Typography sx={{ fontSize: "1.25rem", fontWeight: "medium" }}>
           justj.becerra@gmail.com
         </Typography>
       </Box>
