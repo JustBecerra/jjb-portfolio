809af4192b695a773a56f686467c3d3903ff495e
JustBecerra <justj.becerra@gmail.com>
2024-05-22 11:56:12 -0300

    remodel data passage

--- src/components/Information/index.tsx
+++ src/components/Information/index.tsx
@@ -1,61 +1,58 @@
-"use client";
-import { Box, Typography } from "@mui/material";
-import Image from "next/image";
-import sonriente from "../../../public/sonriente.jpg";
-import { useTranslations } from "next-intl";
+'use client';
+import { Box, Typography } from '@mui/material';
+import Image from 'next/image';
+import sonriente from '../../../public/sonriente.jpg';
+import { useTranslations } from 'next-intl';
+import { useContext } from 'react';
+import PortfolioContext from '@/context/provider';
 
-export default function Information({
-  informationRef,
-}: {
-  informationRef: React.RefObject<HTMLDivElement>;
-}) {
-  const t = useTranslations("home");
+export default function Information() {
+  const { informationRef } = useContext(PortfolioContext);
+  const t = useTranslations('home');
   return (
     <Box
       sx={{
-        width: "100%",
-        height: "40%",
-        display: "flex",
-        flexDirection: { xs: "column", md: "row" },
-        justifyContent: "center",
-        alignItems: "center",
-        gap: "2rem",
-        my: "12rem",
+        width: '100%',
+        height: '40%',
+        display: 'flex',
+        flexDirection: { xs: 'column', md: 'row' },
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: '2rem',
+        my: '12rem',
       }}
       ref={informationRef}
     >
       <Box
         sx={{
-          width: { xs: "350px", md: "500px" },
-          height: { xs: "350px", md: "500px" },
-          position: "relative",
+          width: { xs: '350px', md: '500px' },
+          height: { xs: '350px', md: '500px' },
+          position: 'relative',
         }}
       >
-        <Image src={sonriente} alt="" fill style={{ borderRadius: "1rem" }} />
+        <Image src={sonriente} alt="" fill style={{ borderRadius: '1rem' }} />
       </Box>
       <Box
         sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "flex-start",
-          width: { xs: "80%", md: "50%" },
-          height: "50%",
-          gap: "2rem",
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'flex-start',
+          width: { xs: '80%', md: '50%' },
+          height: '50%',
+          gap: '2rem',
         }}
       >
         <Typography
           sx={{
-            fontWeight: "medium",
-            textAlign: "center",
-            fontSize: "2.5rem",
-            whiteSpace: "normal",
+            fontWeight: 'medium',
+            textAlign: 'center',
+            fontSize: '2.5rem',
+            whiteSpace: 'normal',
           }}
         >
-          {t("welcome")}
+          {t('welcome')}
         </Typography>
-        <Typography sx={{ fontSize: "1.25rem", textAlign: "center" }}>
-          {t("intro")}
-        </Typography>
+        <Typography sx={{ fontSize: '1.25rem', textAlign: 'center' }}>{t('intro')}</Typography>
       </Box>
     </Box>
   );
--- src/components/TechnologiesMain/index.tsx
+++ src/components/TechnologiesMain/index.tsx
@@ -1,42 +1,30 @@
-"use client";
-import { Box } from "@mui/material";
-import React from "react";
-import { TechnologiesCards } from "../TechnologiesCards";
-import { useTranslations } from "next-intl";
-import { TechnologiesList } from "@/misc";
+'use client';
+import { Box } from '@mui/material';
+import React, { useContext } from 'react';
+import { TechnologiesCards } from '../TechnologiesCards';
+import { useTranslations } from 'next-intl';
+import { TechnologiesList } from '@/misc';
+import PortfolioContext from '@/context/provider';
 
-export default function Technologies({
-  technologiesRef,
-}: {
-  technologiesRef: React.RefObject<HTMLDivElement>;
-}) {
-  const t = useTranslations("technologies");
-  const frontendTechnologies = TechnologiesList.filter(
-    (tech) => tech.area === "frontend"
-  );
-  const backendTechnologies = TechnologiesList.filter(
-    (tech) => tech.area === "backend"
-  );
+export default function Technologies() {
+  const { technologiesRef } = useContext(PortfolioContext);
+  const t = useTranslations('technologies');
+  const frontendTechnologies = TechnologiesList.filter((tech) => tech.area === 'frontend');
+  const backendTechnologies = TechnologiesList.filter((tech) => tech.area === 'backend');
   return (
     <Box
       sx={{
-        width: "80%",
-        height: "40%",
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "baseline",
-        my: "12rem",
+        width: '80%',
+        height: '40%',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'baseline',
+        my: '12rem',
       }}
       ref={technologiesRef}
     >
-      <TechnologiesCards
-        technologies={frontendTechnologies}
-        title={t("frontendtech")}
-      />
-      <TechnologiesCards
-        technologies={backendTechnologies}
-        title={t("backendtech")}
-      />
+      <TechnologiesCards technologies={frontendTechnologies} title={t('frontendtech')} />
+      <TechnologiesCards technologies={backendTechnologies} title={t('backendtech')} />
     </Box>
   );
 }
